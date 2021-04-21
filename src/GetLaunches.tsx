9ed56ed3db9da2eba28ae96c8b7a02cdaadb1bce
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-04-21 15:59:51 +0200

    remove "any" type to use interfaces

--- src/GetLaunches.tsx
+++ src/GetLaunches.tsx
@@ -1,14 +1,30 @@
 import { useQuery } from "@apollo/client";
 import { queryGetLaunches } from "./queries";
 
-const GetLaunches = (): any => {
+interface Rocket {
+  rocket_name: string;
+}
+
+interface Links {
+  video_link: string;
+}
+
+interface Launch {
+  launch_date_utc: string;
+  details: string;
+  launch_success: boolean;
+  rocket: Rocket;
+  links: Links;
+}
+
+const GetLaunches = () => {
   const { loading, error, data } = useQuery(queryGetLaunches);
   if (loading) return <p>Loading ...</p>;
   if (error) return <p>Error while fetching datas</p>;
   console.log(data);
   return (
     <>
-      {data.launches.map((launch: any) => (
+      {data.launches.map((launch: Launch) => (
         <div key={launch.launch_date_utc}>
           <p>Launch date :{launch.launch_date_utc}</p>
           <p>Details : {launch.details}</p>
